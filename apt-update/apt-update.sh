c8cb7dfc6671c65efe28e561ac1987de91c4cd69
Samuel Oliver <sam.xyz@outlook.com>
2023-11-27 12:49:24 +0000

    start

---
+++ apt-update/apt-update.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#update all silently
+apt-get -o Dpkg::Options::='--force-confold' --force-yes -fuy dist-upgrade
+
+#Dpkg::Options::='--force-confold' :
+#--force-confold: do not modify the current configuration file, 
+#the new version is installed with a .dpkg-dist suffix. 
+#With this option alone, even configuration files that you have not modified are left untouched. 
+#You need to combine it with --force-confdef to let dpkg overwrite configuration files that you have not modified.
